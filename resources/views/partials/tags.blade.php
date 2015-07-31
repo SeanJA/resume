863082ff629ea071c74a6267ee39965d1d072a6f
Sean Sandy <sean.sandy@gmail.com>
2015-07-31 15:16:07 -0400

    make sure that tags only show up if there are some

--- resources/views/partials/tags.blade.php
+++ resources/views/partials/tags.blade.php
@@ -1,6 +1,8 @@
-<div class="large-12 columns">
-    <strong>Skills / Tech:</strong>
-    <small>
-        @foreach($experience->tags as $i=>$tag)@if($i > 0), @endif<span data-count="{{$tag->count}}">{{$tag->name}}</span>@endforeach
-    </small>
-</div>
+@if($experience->tags->count())
+    <div class="large-12 columns">
+        <strong>Skills / Tech:</strong>
+        <small>
+            @foreach($experience->tags as $i=>$tag)@if($i > 0), @endif<span data-count="{{$tag->count}}">{{$tag->name}}</span>@endforeach
+        </small>
+    </div>
+@endif
