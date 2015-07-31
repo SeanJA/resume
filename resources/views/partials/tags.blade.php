0efd0dd64f3a38a9ab49ff1210f9ff5b802181dd
Sean Sandy <sean.sandy@gmail.com>
2015-07-31 15:36:38 -0400

    only show the tag list if there are tags

--- resources/views/partials/tags.blade.php
+++ resources/views/partials/tags.blade.php
@@ -1,8 +1,8 @@
-@if($experience->tags->count())
+@if($experience->tags()->count() > 0)
     <div class="large-12 columns">
         <strong>Skills / Tech:</strong>
         <small>
-            @foreach($experience->tags as $i=>$tag)@if($i > 0), @endif<span data-count="{{$tag->count}}">{{$tag->name}}</span>@endforeach
+            @foreach($experience->tags() as $i=>$tag)@if($i > 0), @endif<span data-count="{{$tag->count}}">{{$tag->name}}</span>@endforeach
         </small>
     </div>
 @endif
